1cf33e0079577204caf8be30b2a325be5234d497
yankagames123 <148909328+yankagames123@users.noreply.github.com>
2023-11-24 00:14:32 -0300

    projeto-1

--- sketch.js
+++ sketch.js
@@ -1,1 +1,13 @@
-
+function setup() {
+    createCanvas(400, 400);
+    background("purple");
+  }
+  
+  function draw() {
+    stroke("pink");
+    fill ("gray");
+    
+    if(mouseIsPressed) {
+    rect (mouseX,mouseY,20,20);
+  }
+     }
